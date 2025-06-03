24dcdbbc7e6ec356180cb247918f80f5dcb5e913
rahmiisahdalii <rahmi475@gmail.com>
2025-06-03 14:58:32 +0300

    7133 port update

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -15,7 +15,7 @@ class DioClient {
 
   DioClient._internal()
       : _dio = Dio(BaseOptions(
-          baseUrl: 'http://193.111.78.145:80/Auth', // Base URL
+          baseUrl: 'http://193.111.78.145:7133/Auth', // Base URL
           connectTimeout:
               Duration(milliseconds: 5000), // 5 saniye bağlantı zaman aşımı
           receiveTimeout:
